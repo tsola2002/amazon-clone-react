c216480df016de76a13614659716ca1afd6a25b4
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2020-10-16 02:58:36 +0100

    initialized project & added firebase config files

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDpiuMjkN3Lyw_up2Niwe81vX9B72MLKuI",
+    authDomain: "clone-bdfbe.firebaseapp.com",
+    databaseURL: "https://clone-bdfbe.firebaseio.com",
+    projectId: "clone-bdfbe",
+    storageBucket: "clone-bdfbe.appspot.com",
+    messagingSenderId: "35277267337",
+    appId: "1:35277267337:web:8bf1d3457382baeb9a0158"
+  };
